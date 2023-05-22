a749fa0dad8af8ef7a7db1d9ca69ef9859865a1d
Gilson Marcos <gilsontpaim@gmail.com>
2023-05-21 21:34:02 -0300

    Update routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,45 +1,51 @@
-import ErrorNotFound from 'pages/ErrorNotFound.vue';
-import MainLayout from 'layouts/MainLayout.vue';
-import IndexPage from '../pages/IndexPage.vue';
+import ErrorNotFound from "pages/ErrorNotFound.vue";
+import MainLayout from "layouts/MainLayout.vue";
+import IndexPage from "../pages/IndexPage.vue";
 
 const routes = [
   {
-    path: '/menu',
+    path: "/menu",
     component: MainLayout,
-    children: ({
-      path: 'menu',
+    children: {
+      path: "menu",
       component: IndexPage,
-    }),
+    },
   },
   {
-    path: '/encomendas',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{
-      path: '/encomendas',
-      name: 'encomendas',
-      component: () => import('pages/TelaEscolha.vue'),
-    },
+    path: "/main",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      {
+        path: "/telaescolha",
+        name: "encomendas",
+        component: () => import("pages/TelaEscolha.vue"),
+      },
     ],
   },
   {
-    path: '/cadastrousuario',
-    name: 'Cadastro de Usuario',
-    component: () => import('pages/cadastroUsuario.vue'),
+    path: "/cadastrousuario",
+    name: "Cadastro de Usuario",
+    component: () => import("pages/cadastroUsuario.vue"),
   },
   {
-    path: '/',
-    name: 'Login Usuario',
-    component: () => import('pages/LoginPageMorador.vue'),
+    path: "/",
+    name: "Login Usuario",
+    component: () => import("pages/LoginPageMorador.vue"),
   },
   {
-    path: '/encomendas',
-    name: 'encomendas',
-    component: () => import('pages/encomendasRecebidas.vue'),
+    path: "/encomendas",
+    name: "encomendas",
+    component: () => import("pages/encomendasRecebidas.vue"),
+  },
+  {
+    path: "/cadastroencomendas",
+    name: "Cadastro de encomendas",
+    component: () => import("pages/cadastroEncomendas.vue"),
   },
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
+    path: "/:catchAll(.*)*",
     component: ErrorNotFound,
   },
 ];
